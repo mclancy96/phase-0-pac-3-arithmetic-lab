6e70e1e08ce1e99c665c5df57b041a57ce56c4bf
Mike Clancy <mclancy96@gmail.com>
2025-02-18 22:08:54 -0500

    adds +-*/

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
